9e7161c4b2f73567a3adf3621bc509976a82bb5e
ravishatcusat <46592833+ravishatcusat@users.noreply.github.com>
2020-10-26 02:02:44 +0530

    Spring initilizar

---
+++ src/main/java/com/ravish/springBootdbapi/SpringBootDbApiApplication.java
@@ -0,0 +1,13 @@
+package com.ravish.springBootdbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravish/springBootdbapi/SpringBootDbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravish.springBootdbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
