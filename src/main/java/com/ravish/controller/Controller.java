cc5bc8fd786ae8cbccab1401c9c1c3d41083fcc6
ravishatcusat <46592833+ravishatcusat@users.noreply.github.com>
2020-10-27 03:00:57 +0530

    request model

--- src/main/java/com/ravish/controller/Controller.java
+++ src/main/java/com/ravish/controller/Controller.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ravish.model.request.UserDetailsRequestModel;
 
 @RestController
 @RequestMapping("api")
@@ -17,7 +20,7 @@ public class Controller {
 	}
 	
 	@PostMapping
-	public String createUser() {
+	public String createUser(@RequestBody UserDetailsRequestModel userDetails) {
 		return "Create user was called";
 	}
 	
